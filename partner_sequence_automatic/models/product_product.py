8a336736f68ebffeeda45e7f7dc53c4738524f29
RAG Solutions <124674380+Mohlahrech@users.noreply.github.com>
2025-06-01 23:31:11 +0100

    Delete partner_sequence_automatic directory

--- partner_sequence_automatic/models/product_product.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ProductProduct(models.Model):
-    _inherit = "product.product"
-
-    bd_lofric = fields.Boolean(string="Affiché au CRM", default=False)
--- partner_sequence_automatic/models/res_users.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ResUsers(models.Model):
-    _inherit = "res.users"
-
-    pfp = fields.Boolean(string="PFP", default=False)
