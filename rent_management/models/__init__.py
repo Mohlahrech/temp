568d227a6b25a13d2623c0984cfae0228c0fd3ee
RAG Solutions <124674380+Mohlahrech@users.noreply.github.com>
2025-06-21 14:37:31 +0100

    Delete rent_management directory

--- rent_management/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import sale_order, config, res_partner, convertion, account_move,sal_order_prolongation
-from . import historique_maintenance,product_template
-from . import rent_maintenances
--- rent_management/models/rent_maintenances.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class RentMaintenance(models.Model):
-    _name = 'rent.maintenance'
-    _description = 'Maintenance Types'
-
-    name = fields.Char(string="Maintenance Type", required=True)
-    code = fields.Char(string="Internal Code", required=True, index=True)
-    mileage_to_notify = fields.Integer(string="Mileage for Notification")
